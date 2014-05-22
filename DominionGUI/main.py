da93a663e64c539cc8ee65f01cfa17aeecbd1dc6
zitin <ari.zitin@gmail.com>
2014-05-22 17:51:57 -0400

    started GUI outline

---
+++ DominionGUI/main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
+
+class DominionApp(App):
+	def build(self):
+
+class DominionGame(Widget):
+
