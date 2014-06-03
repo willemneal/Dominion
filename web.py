4dd0db8db953b1a44df616b2e2146330d9ec1250
Willem Wyndham <willem.neal@gmail.com>
2014-06-03 18:14:51 -0400

    Added a Dictionary of Games

--- web.py
+++ web.py
@@ -3,25 +3,26 @@ app = Flask(__name__)
 from game import Game,base,playerList
 from flask import render_template
 
+Games = {}
 @app.route('/')
 def index():
-    global G
-    G = Game(playerList,[base])
+    Games[1] = Game(playerList,[base])
     return 'Game Has Started'
 
-@app.route('/player/<name>/hand')
-def player(name):
-    global G
-    return render_template('/hand.html',name =name, hand=G.playerDict[name].hand)
-
-@app.route('/supply')
-def supply():
-	global G
-	return render_template('/supply.html', victoryCards = G.supply.victoryCards, 
-										   treasureCards= G.supply.treasureCards,
-										   kingdomCards=  G.supply.kingdomCards,
-										   miscCards=     G.supply.miscCards,
-										   nonSupplyCards=G.supply.nonSupplyCards)
+@app.route('/<gameid>/player/<name>/hand')
+def player(gameid,name):
+    if Games[gameid]:
+        return render_template('/hand.html',name =name, hand=G.playerDict[name].hand)
+    return redirect(url_for('/'))
+@app.route('/<gameid>/supply')
+def supply(gameid):
+    if Games[gameid]:
+       return render_template('/supply.html',  victoryCards   = G.supply.victoryCards, 
+                                               treasureCards  = G.supply.treasureCards,
+                                               kingdomCards   = G.supply.kingdomCards,
+                                               miscCards      = G.supply.miscCards,
+                                               nonSupplyCards = G.supply.nonSupplyCards)
+    return redirect(url_for('/'))
 
 if __name__ == '__main__':
     app.run(debug=True)
